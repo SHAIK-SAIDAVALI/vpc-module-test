a6fad241fe43bb626991e2affb08e3642e0f0307
shaik saidavali <shaiksaidavali223@gmail.com>
2026-01-30 16:45:25 +0530

    vpc-module

---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.16.0"
+    }
+  }
+  backend "s3" {
+    bucket = "daws-86a-store"
+    key    = "vpc-module-demo"
+    region = "us-east-1"
+    use_lockfile = true
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "vpc_cidr" {
+    default = "10.0.0.0/16"
+  
+}
+variable "project_name" {
+    default = "roboshop"
+  
+}
+variable "environment" {
+    default = "dev"
+  
+}
+variable "vpc_tags" {
+    default = {
+        purpose = "vpc-module-test"
+        dontDelete = "true"
+    }
+  
+}
+variable "public_subnet_cidrs" {
+    default = ["10.0.1.0/24","10.0.2.0/24"]
+  
+}
